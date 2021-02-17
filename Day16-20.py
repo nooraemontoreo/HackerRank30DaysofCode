eee44d5b1b3fd9f81b67ed36013db6a09a1ab973
nooraemontoreo <70823397+nooraemontoreo@users.noreply.github.com>
2021-02-17 17:47:19 +0500

    Update Day16-20.py

--- Day16-20.py
+++ Day16-20.py
@@ -1,16 +1,16 @@
 #DAY 16                Exception handling
 
 #Read a string, , and print its integer value; if cannot be converted to an integer, print Bad String.
-Note: You must use the String-to-Integer and exception handling constructs built into your submission language. If you attempt to use loops/conditional statements, you will get ascore.
+#Note: You must use the String-to-Integer and exception handling constructs built into your submission language. If you attempt to use loops/conditional statements, you will get ascore.
 
-Sample Input 0
-3
-Sample Output 0
-3
-Sample Input 1
-za
-Sample Output 1
-Bad String
+#Sample Input 0
+#3
+#Sample Output 0
+#3
+#Sample Input 1
+#za
+#Sample Output 1
+#Bad String
 
 import sys
 S = input().strip()
